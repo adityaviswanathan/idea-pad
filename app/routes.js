abb6bd2df651dca36b8b4170b2e82ce374fefa89
Aditya Viswanathan <aditya@adityaviswanathan.com>
2014-06-24 23:14:32 -0500

    figured out how to manage user data

--- app/routes.js
+++ app/routes.js
@@ -4,7 +4,10 @@ module.exports = function(app, passport) {
 
 	// show the home page (will also have our login links)
 	app.get('/', function(req, res) {
-		res.render('index.ejs');
+		console.log(req.session);
+		res.render('index.ejs', {
+			user: req.user
+		});
 	});
 
 	// PROFILE SECTION =========================
