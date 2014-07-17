4ce0dde7ad1462c69e5ebee4381dd8827c2b5199
Aditya Viswanathan <aditya@adityaviswanathan.com>
2014-07-17 11:48:28 -0500

    fixed sidebar and progress bar

--- public/client.js
+++ public/client.js
@@ -1,14 +1,33 @@
 function newField(event) {
 	if(event.which == 13) {
-		console.log('ENTER PRESSED');
 		var input_field = '<li><input type="text" name="ip-form-data-source" onkeydown="return newField(event)"></li>';
 		$(event.target).parent().after(input_field);
 		$(event.target).parent().next().children('input').focus();
 	}
 	if(event.which == 8 && !($(event.target).val())) {
-		console.log('BACKSPACE FIRED');
 		$(event.target).parent().prev().children('input').focus();
 		if($(event.target).parent().siblings().length) $(event.target).parent().remove();	
 		event.preventDefault();
 	}
 }
+
+$("input[name='ip-form-stream']").on('click', function() {
+	var value = $(this).attr('class');
+
+    switch(value) {
+    	case 'ip-form-stream-unit':
+    		$(this).siblings('.ip-form-stream-display').html('sell each at&nbsp<input type="number" name="ip-form-stream-unit-price">');
+    		break;
+    	case 'ip-form-stream-subs':
+    		$(this).siblings('.ip-form-stream-display').html('users will pay&nbsp<input type="number" name="ip-form-stream-subs-price">&nbspevery&nbsp<select><option value="day">day</option><option value="week">week</option><option value="month">month</option><option value="year">year</option></select>');
+    		break;
+    	case 'ip-form-stream-comm':
+    		$(this).siblings('.ip-form-stream-display').html('for each&nbsp<input type="text" class="ip-form-stream-comm-action">, we will earn&nbsp<input type="number" class="ip-form-stream-comm-price">');
+    		break;
+    	case 'ip-form-stream-ads':
+    		$(this).siblings('.ip-form-stream-display').html('there is a definite audience of&nbsp<input type="text" class="ip-form-stream-ads-num">&nbspearly users');
+    		break;
+    	case 'ip-form-stream-other':
+    		$(this).siblings('.ip-form-stream-display').html('one sentence on how it\'ll make money:&nbsp<input type="text" class="ip-form-stream-other-sentence">');	
+    }    
+});
